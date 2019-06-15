64e1bfa094c63ccbff76c1bac16d7c88cadea402
L Sherry <lesser.sherry@protonmail.com>
2019-06-15 23:14:14 +0200

    Initial commit.
    the mighty jsontr.py still need to be rewritten.

---
+++ common.py
@@ -0,0 +1,180 @@
+#!/bin/echo This file is not meant to be executable:
+
+import os, os.path, json
+
+load_json = lambda p: json.load(open(p, encoding="utf-8"))
+save_json_to_fd = lambda fd, v: json.dump(v, fd, indent=8,
+                                          separators=(',',': '),
+                                          ensure_ascii=False)
+
+save_json = lambda p, v: save_json_to_fd(open(p, 'w', encoding="utf-8"), v)
+
+def walk_json_inner(json, dict_path = None, reverse_path = None):
+    if dict_path is None:
+        dict_path = []
+    if reverse_path is None:
+        reverse_path = []
+    if (yield json, dict_path, reverse_path) is False:
+        return
+    if isinstance(json, dict):
+        iterable = json.items() # not sorted
+    elif isinstance(json, list):
+        iterable = ((str(i), value) for i,value in enumerate(json))
+    else:
+        return
+    for k,v in iterable:
+        dict_path.append(k)
+        reverse_path.append(json)
+        yield from walk_json_inner(v, dict_path, reverse_path)
+        popped_k = dict_path.pop()
+        popped_json = reverse_path.pop()
+        assert popped_k is k and popped_json is json
+
+def walk_json_for_langlabels(json, lang_to_check):
+    iterator = walk_json_inner(json, file_path)
+    for value, dict_path, reverse_path in iterator:
+        if isinstance(value, dict) and lang_to_check in value:
+            iterator.send(False)
+            yield value, dict_path, value, reverse_path
+
+def walk_langfile_json(json, lang):
+    for value, dict_path, reverse_path in walk_json_inner(json):
+        if len(dict_path) == 1:
+            continue
+        elif not isinstance(value, str):
+            continue
+        yield { lang: value }, dict_path, reverse_path
+
+def walk_files(base_path, sort = True):
+    for dirpath, subdirs, filenames in os.walk(base_path):
+        for name in sorted(filenames) if sort else filenames:
+            usable_path = os.path.join(dirpath, name)
+            rel_path = os.path.relpath(usable_path, base_path)
+            yield usable_path, rel_path
+
+def walk_assets_for_translatables(path, orig_lang,
+                                  path_filter = lambda x: True):
+    base_path = os.path.join(get_assets_path(path), "data")
+
+    for usable_path, rel_path in walk_files(base_path):
+        file_path = rel_path.split(os.sep)
+        if not path_filter(file_path):
+            continue
+        json = load_json(usable_path)
+        if rel_path.startswith("lang"):
+            if orig_lang not in rel_path:
+                continue
+            iterable = walk_langfile_json(json, orig_lang)
+        else:
+            iterable = walk_json_for_langlabels(json, orig_lang)
+        for value, dict_path, reverse_path in iterable:
+            yield value, (file_path, dict_path), reverse_path
+
+def get_data_by_dict_path(json, dict_path):
+    for component in dict_path:
+        if isinstance(json, list):
+            try:
+                json = json[int(component)]
+            except:
+                return None
+        else:
+            json = json.get(component)
+            if json is None:
+                return None
+    return json
+
+
+def serialize_dict_path(file_path, dict_path):
+    # FIXME: we need a better encoding maybe.
+    # eg: escape / in keys with \, escape \ with \ as well, and find
+    # a way to handle json files not having a .json extension.
+    assert file_path[-1][-5:] == ".json"
+    return "%s/%s"%("/".join(file_path), "/".join(dict_path))
+
+def unserialize_dict_path(dict_path_str):
+    l = dict_path_str.split('/')
+    for index, value in enumerate(l):
+        if value.endswith('.json'):
+            return (l[:index+1], l[index+1:])
+    raise ValueError("cannot unserialize that")
+
+
+
+def get_assets_path(path):
+    realpath = os.path.realpath(path)
+    realsplit = realpath.split(os.sep)
+    try:
+        index = realsplit.index("assets")
+    except ValueError:
+        maybe = os.path.join(path, "assets")
+        if os.path.isdir(maybe):
+            return maybe
+        else:
+            raise ValueError("could not find game assets." 
+                             " Searched in %s and %s/assets"%(path, path))
+    else:
+        return path + ("%s.."%os.sep) * (len(realsplit) - index - 1)
+
+class sparse_dict_path_reader:
+    def __init__(self, gamepath, lang):
+        self.base_path = os.path.join(get_assets_path(gamepath), "data")
+        self.last_loaded = None
+        self.last_data = None
+        self.lang = lang
+
+    def get(self, file_path, dict_path):
+        if self.last_loaded != file_path:
+            usable_path = os.path.join(self.base_path, os.sep.join(file_path))
+            try:
+                self.last_data = load_json(usable_path)
+            except Exception as e:
+                print("Cannot find game file:", "/".join(file_path), ':',
+                      str(e))
+                self.last_data = {}
+            self.last_loaded = file_path
+
+        ret = get_data_by_dict_path(self.last_data, dict_path)
+        if file_path[0] != "lang" and ret is not None:
+            assert isinstance(ret, dict)
+            return ret.get(self.lang)
+        return ret
+
+    def get_str(self, file_dict_path_str):
+        file_path, dict_path = unserialize_dict_path(file_dict_path_str)
+        return self.get(file_path, dict_path)
+
+
+
+def transform_file_or_dir(input_path, output_path):
+    """Browse all files in input_file and transform them into output_path.
+
+    e.g. you can reimplement cp -r with:
+    for a,b in transform_file_or_dir(arg1, arg2):
+        open(a, "w").write(open(b).read()
+
+    It supports the following cases:
+    input | output | result
+    -----------------------
+    file  | file   | yield input, output, None
+    file  | dir    | yield input, output + basename(input), None
+    dir   | dir    | will walk the input directory, and
+                     yield (input_file, output_file, relative_path) for each
+                     found file.  This will also recreate the output directory
+                     along the way.
+    dir   | file   | will fail.
+    """
+    if os.path.isfile(input_path):
+        if os.path.isdir(output_path):
+            filename = os.path.basename(input_path)
+            output_path = os.path.join(output_path, filename)
+        yield input_path, output_path, None
+    elif os.path.isdir(input_path):
+        olddirpath = None
+        for usable_path, rel_path in walk_files(input_path):
+            dirpath = os.path.dirname(rel_path)
+            if olddirpath != dirpath:
+                olddirpath = dirpath
+                os.makedirs(os.path.join(output_path, dirpath), exist_ok=True)
+            yield usable_path, os.path.join(output_path, rel_path), rel_path
+    else:
+        raise ValueError("input '%s' does not exist"%input_path)
---
+++ packfile.py
@@ -0,0 +1,366 @@
+#!/usr/bin/python3
+
+import base64
+import hmac
+import hashlib
+import os
+import sys
+
+try:
+    # The only thing not part of python
+    import Crypto.Cipher.AES
+except:
+    print("pycrypto not found. crypto operations will fail !")
+
+import common
+
+class Encraption:
+    """Encrapt stuff to protect against lawyers
+    
+    Encrapt stuff so that lawyers cannot tell us we're distributing copyrighted
+    stuff in the 'orig' and derived works in 'text' to people that don't
+    have the original material.
+
+    Any lawless thug can workaround this encraption, that's not the problem.
+
+    List of things that we do that would horify a crypto researcher:
+    - md5 of low entropy text as key derivation
+    - key == iv
+    - mac key = aes key
+    - vulnerable reimplementation of pkcs7
+    - encrypt and mac
+    """
+    def __init__(self, orig_text):
+        self.key = hashlib.md5(orig_text.encode('utf-8')).digest()
+
+    # no pkcs7 in pycrypto ?
+    @staticmethod
+    def _pkcs7pad(pad_length):
+        return bytes(pad_length for x in range(pad_length))
+
+    def _aes(self):
+        return Crypto.Cipher.AES.new(self.key, Crypto.Cipher.AES.MODE_CBC,
+                                     self.key)
+
+    def encrapt_text(self, text):
+        text = text.encode("utf-8")
+        pad_length = 16 - len(text) % 16
+        padded_text = text + self._pkcs7pad(pad_length)
+        ciphertext = self._aes().encrypt(padded_text)
+        return base64.b64encode(ciphertext).decode('ascii')
+
+    def decrapt_text(self, ciphertext):
+        ciphertext = base64.b64decode(ciphertext.encode('ascii'))
+        # hey, i only need to defend against lawyers.
+        padded_text = self._aes().decrypt(ciphertext)
+        pad_length = padded_text[-1]
+        if not (0 < pad_length <= 16
+                and padded_text[-pad_length:] == self._pkcs7pad(pad_length)):
+            print("ciphertext:", repr(ciphertext))
+            print("decrapt:", repr(padded_text), "with key", repr(self.key))
+            print("FAIIIIIIIIIIIIIIIIL")
+            raise ValueError("bad pcks7 padding")
+        #print("decrapt[:-pad]:", repr(padded_text[:-pad_length]))
+        return padded_text[:-pad_length].decode('utf-8')
+
+    def mac_text(self, text):
+        # didn't find anything else but md5 in the loaded JS.
+        mac = hmac.digest(self.key, text.encode('utf-8'), 'md5')
+        return base64.b64encode(mac).decode('ascii')
+
+    @staticmethod
+    def encrapt_trans(trans_object):
+
+        e = Encraption(trans_object['orig'])
+        text = trans_object['text']
+
+        ret = { "ciphertext": e.encrapt_text(text), "mac": e.mac_text(text) }
+
+        quality = trans_object.get('quality')
+        if quality is not None:
+            ret["quality"] = quality
+        if "reason" in trans_object: # was equivalent to 'note'
+            raise ValueError("Throw out this old file, bro")
+        note = trans_object.get('note')
+        if note is not None:
+            ret["ciphernote"] = e.encrapt_text(note)
+        return ret
+
+    @staticmethod
+    def decrapt_trans(trans_object, orig):
+        if "text" in trans_object:
+            raise ValueError("It's not encrypted ?")
+        e = Encraption(orig)
+        text = e.decrapt_text(trans_object["ciphertext"])
+        mac = trans_object.get("mac")
+        if mac is not None and mac != e.mac_text(text):
+            raise ValueError("MAC mismatch")
+        ret = { 'orig': orig, 'text': text }
+        quality = trans_object.get("quality")
+        if quality is not None:
+            ret["quality"] = quality
+        ciphernote = trans_object.get("ciphernote")
+        if ciphernote is not None:
+            ret["note"] = e.decrapt_text(ciphernote)
+        return ret
+
+def do_encrapt(args):
+    try:
+        game_reader = common.sparse_dict_path_reader(args.gamedir,
+                                                     args.fromlocale)
+    except Exception as e:
+        game_reader = None
+        print("Cannot find game assets:", str(e),
+              " continuing by trusting the packfiles")
+
+    error = False
+    iterator = common.transform_file_or_dir(args.inputpath, args.outputpath)
+    for input_file, output_file, rel_path in iterator:
+        json = common.load_json(input_file)
+        result = {}
+        for file_dict_path_str, value in json.items():
+            orig_in_file = value.get('orig')
+            orig_in_game = None
+            if game_reader is not None:
+                orig_in_game = game_reader.get_str(file_dict_path_str)
+                if orig_in_file is None:
+                    value['orig'] = orig_in_game
+                elif orig_in_game is not None and orig_in_game != orig_in_file:
+                    orig_in_game = None
+                if orig_in_game is None:
+                    print("Not encrapting stale translation at",
+                          file_dict_path_str)
+                    error = True
+                    continue
+            result[file_dict_path_str] = Encraption.encrapt_trans(value)
+        common.save_json(output_file, result)
+    if error:
+        sys.exit(1)
+
+def do_decrapt(args):
+    game_reader = common.sparse_dict_path_reader(args.gamedir, args.fromlocale)
+    iterator = common.transform_file_or_dir(args.inputpath, args.outputpath)
+    error = False
+    for input_file, output_file, _ in iterator:
+        try:
+            json = common.load_json(input_file)
+        except Exception as e:
+            print("Cannot read", input_file, ":", str(e))
+            continue
+        result = {}
+        for file_dict_path_str, value in json.items():
+            #print("filedictpath", repr(file_dict_path_str))
+            orig = game_reader.get_str(file_dict_path_str)
+            if orig is None:
+                print("Cannot read original translation at", file_dict_path_str)
+                error = True
+                continue
+            try:
+                decrapted = Encraption.decrapt_trans(value, orig)
+            except Exception as e:
+                print("Cannot decrypt (is translation stale?):", str(e),
+                      "at", file_dict_path_str)
+                error = True
+                continue
+            result[file_dict_path_str] = decrapted
+        common.save_json(output_file, result)
+    if error:
+        sys.exit(1)
+
+def do_make_mapfile(args):
+    json = common.load_json(args.bigpack)
+    result = {}
+    prefix = args.prefix
+    if prefix and not prefix.endswith('/'):
+        prefix += '/'
+    for file_dict_path_str in json.keys():
+        file_path, _ = common.unserialize_dict_path(file_dict_path_str)
+        path = "/".join(file_path)
+        result[path] = prefix + path
+    common.save_json(args.mapfile, result)
+
+def do_split(args):
+    big_pack = common.load_json(args.bigpack)
+    map_file = common.load_json(args.mapfile)
+    results = {}
+    old_file_path_str = None
+    error = False
+    for file_dict_path_str, trans in big_pack.items():
+        file_path, _ = common.unserialize_dict_path(file_dict_path_str)
+        file_path_str = "/".join(file_path)
+        to_file_str = map_file.get(file_path_str)
+        if to_file_str is None:
+            print("missing pack reference for", file_path_str)
+            error = True
+
+        results.setdefault(to_file_str, {})[file_dict_path_str] = trans
+
+    if error:
+        print("Aborting...")
+        sys.exit(1)
+
+    for to_file_str, smaller_pack in results.items():
+        to_file = to_file_str.split('/')[args.strip:]
+        if not to_file:
+            print("strip parameter", args.strip, "is too large for path",
+                  to_file_str)
+            print("Aborting...")
+            sys.exit(1)
+        
+        actual_dir = os.path.join(args.outputpath, os.sep.join(to_file[:-1]))
+        os.makedirs(actual_dir, exist_ok=True)
+        common.save_json(os.path.join(actual_dir, to_file[-1]), smaller_pack)
+
+
+def do_merge(args):
+    big_result = {}
+    error = False
+    for usable_path, _ in common.walk_files(args.inputpath):
+        for s, value in common.load_json(usable_path).items():
+            if big_result.setdefault(s, value) != value:
+                print("Multiple different value found for", s)
+                error = True
+    if error:
+        print("Aborting...")
+        sys.exit(1)
+    common.save_json(args.bigpack, big_result)
+
+
+def do_diff_langfile(args):
+    from_json = common.load_json(args.fileorig)
+    to_json = common.load_json(args.filetrans)
+    if "filename" in args and args.filename is not None:
+        file_path = args.filename.split('/')
+    else:
+        file_path = ["lang", "sc", os.path.basename(args.fileorig)]
+    result = {}
+    iterator = common.walk_langfile_json(from_json, "orig")
+    for trans, dict_path, _ in iterator:
+        text = common.get_data_by_dict_path(to_json, dict_path)
+        if text is None:
+            continue
+        trans['text'] = text
+        result[common.serialize_dict_path(file_path, dict_path)] = trans
+    if args.resultfile == '-':
+        common.save_json_to_fd(sys.stdout, result)
+    else:
+        common.save_json(args.resultfile, result)
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser(description="Command to manage pack files\n")
+    parser.add_argument('--game-dir', '-g', metavar="directory",
+                        default='.', dest="gamedir",
+                        help="""Location of the installed game's assets/
+                        directory. Any subdirectory of it is also accepted.  
+                        Search around the current directory by default.""")
+    parser.add_argument('--from-locale', '-l', metavar="locale",
+                        default="en_US", dest="fromlocale",
+                        help="""Locale used for origin, defaults to en_US.""")
+    parser.add_argument('--map-file', '-m', metavar="file", dest="mapfile",
+                        default="map_file.json",
+                        help="""Location of the file containing the mapping
+                        from original game files to location of pack file
+                        with the translated texts""")
+
+
+    def add_stuffpath(stuff, parser, **kw):
+        kw.setdefault("metavar", "<%s dir or file>"%stuff)
+        parser.add_argument("%spath"%stuff, **kw)
+
+    add_inputpath = lambda x,**kw: add_stuffpath("input", x, **kw)
+    add_outputpath = lambda x,**kw: add_stuffpath("output", x, **kw)
+    add_bigpack = lambda x, halp: x.add_argument("bigpack",
+                                                 metavar="<big pack>",
+                                                 help=halp)
+
+
+    subparsers = parser.add_subparsers(metavar="COMMAND", required=True)
+    encrapt = subparsers.add_parser(
+            'encrapt', help="encrypt one or multiple packfiles",
+            description="""read one or multiple pack files and write the
+                           encrapted output.  If the input is a directory, the
+                           output must be a directory.""")
+    add_inputpath(encrapt, help="""unencrypted pack file or directory 
+                                containing pack files""")
+    add_outputpath(encrapt, help="""where to write encrapted pack file(s)""")
+    encrapt.set_defaults(func=do_encrapt)
+
+
+    decrapt = subparsers.add_parser(
+            'decrapt', help="decrypt one or multiple packfiles",
+            description="""read one or multiple pack files and write the
+                           decrapted output.  This command requires --gamedir
+                           and --from-locale to be set correctly.  If the input
+                           is a directory, the output must be a directory.""")
+    add_inputpath(decrapt, help="""encrapted pack file or directory containing
+                                   pack files""")
+    add_outputpath(decrapt, help="""where to write decrapted pack file(s)""")
+    decrapt.set_defaults(func=do_decrapt)
+
+    make_map = subparsers.add_parser(
+            'mkmap', help="create a default map file for 'split'",
+            description="""Read a big packfile and write a map file
+                           with sensible default values.  The map file can then
+                           be customized manually afterward, or can be used
+                           with split as-is.""")
+    add_bigpack(make_map, "pack file to use as a template to create the map")
+    make_map.add_argument("--prefix", default="", help="""prefix to use before
+                          packs.  e.g. if specifing mods/mymod/packs, then all
+                          small pack will be stored as a subdirectory of
+                          mods/mymod/packs/""")
+    make_map.set_defaults(func=do_make_mapfile)
+
+    split = subparsers.add_parser(
+            'split', help="split a big packfile into small ones",
+            description="""Read a big packfile and a map file and
+                           write several smaller packfile, controlled by the
+                           map file""")
+
+    add_bigpack(split, "pack file to split")
+    add_stuffpath("output", split, metavar="<output dir>",
+                  help="""where to write the smaller packs, according to the
+                          map file""")
+    split.add_argument("--strip", "-p", type=int, default=0,
+                        help="""strip this amount of directories before writing
+                        to the output. e.g. if the map file references
+                        mods/mymod/packs/a, then --strip=2 will write it as
+                        packs/a in the output directory.""")
+    split.set_defaults(func=do_split)
+
+    merge = subparsers.add_parser(
+            'merge', help="merge several packfiles into a big one",
+            description="""Merge all packfiles in a directory into a 
+                           bigger one.""")
+    add_inputpath(merge, metavar="<input dir>",
+                  help="""Where to search for packfiles""")
+    add_bigpack(merge, """Where to write the big packfile""")
+    merge.set_defaults(func=do_merge)
+
+    difflang = subparsers.add_parser(
+            'difflang', help="Calculate differences between two lang files",
+            description="""Given two lang files, calculate the difference as
+                           a pack file and write the output.  Lang files are
+                           files that typically resides under the lang/
+                           directory.""")
+    difflang.add_argument("--file-path",
+                          dest="filename", metavar="<path of original name",
+                          help="""Path to use to refer to the difference in the
+                          output. The default is to use 'lang/sc/<filename>'
+                          where filename is the name of <original file>""")
+    difflang.add_argument("fileorig", metavar="<original file>",
+                          help="""Original file to use as starting point""")
+    difflang.add_argument("filetrans", metavar="<translated file>",
+                          help="""Translated original file""")
+    difflang.add_argument("resultfile", metavar="<output file>",
+                          help="""Where to write the output.  If '-', then
+                                  output to the standard output""")
+    difflang.set_defaults(func=do_diff_langfile)
+
+    result = parser.parse_args()
+    print(vars(result))
+    result.func(result)
+
+
+if __name__=="__main__":
+    parse_args()
